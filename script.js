ee7c5b675066580ae05d2b0b9211e585fe70e711
Ana <aceyunus@gmail.com>
2021-08-08 03:29:16 +0700

    content mockup

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
+$(document).ready(function() {
 
-$(document).ready(function () {
+    $('.play-stop').click(function() {
+        if (!$(this).hasClass('play')) {
+            $(this).attr('src', 'asset/play-48.png');
+            $(this).addClass('play')
+            $("#stream")[0].pause()
+                // $('.cycle-slideshow').cycle('stop');
+        } else {
+            $(this).attr('src', 'asset/stop-48.png');
+            $(this).removeClass('play')
+            $("#stream")[0].play()
+                // $('.cycle-slideshow').cycle('resume');
+        }
+    });
 
-  $('.play-stop').click(function () {
-    if (!$(this).hasClass('play')) {
-      $(this).attr('src', 'asset/play-48.png');
-      $(this).addClass('play')
-      $("#stream")[0].pause()
-      // $('.cycle-slideshow').cycle('stop');
-    } else {
-      $(this).attr('src', 'asset/stop-48.png');
-      $(this).removeClass('play')
-      $("#stream")[0].play()
-      // $('.cycle-slideshow').cycle('resume');
+
+    $('#volume').on('change', setVolume);
+
+
+    function setVolume(e) {
+        var volume = e.target.value;
+        console.log(volume);
+        $("#stream")[0].volume = volume;
     }
-  });
 
-  $('#volume').on('change', setVolume);
 
-  function setVolume(e) {
-    var volume = e.target.value;
-    console.log(volume);
-    $("#stream")[0].volume = volume;
-  }
+    function getStream() {
 
+    }
 });
