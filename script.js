1b84ab1bf77a8fb2cafb8bd7a55205c1973ea577
Ana <aceyunus@gmail.com>
2021-08-22 02:45:08 +0700

    add background transition

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 $(document).ready(function () {
 
+  var bg = [
+    'asset/1.png',
+    'asset/2.jpg',
+    'asset/3.jpeg',
+    'asset/4.jpeg',
+    // 'asset/5.jpg',
+  ];
+
+  // var Transition = 1000;
+
+  $('.fi').css({
+    'background-image': 'url(' + bg[bg.length - 1] + ')',
+    'opacity': '0.5',
+    'background-size': 'cover',
+    'background-repeat': 'no-repeat',
+    'margin': 'auto',
+    'background-position': 'center'
+  });
+
+  window.setInterval(
+    function () {
+      img = bg.shift();
+      bg.push(img);
+
+      var $Backgrounds = $('.fi');
+      $Backgrounds.eq(1).hide(0).css({
+        'background-image': 'url(' + img + ')'
+      }).fadeIn(2000);
+
+      $Backgrounds.eq(0).show(0).fadeOut(2000, function () {
+        $(this).show(0).css({
+          'background-image': 'url(' + img + ')',
+          'opacity': '0.5',
+          'background-size': 'cover',
+          'background-repeat': 'no-repeat',
+          'margin': 'auto',
+          'background-position': 'center'
+        });
+        $Backgrounds.eq(1).hide(0);
+      });
+    }, 4000
+  );
+
   $audio = $("#stream")[0]
   getStream();
 
@@ -7,7 +50,7 @@ $(document).ready(function () {
     if (!$(this).hasClass('goPlay')) {
       $(this).addClass('goPlay')
       $audio.pause()
-      $("#stream").prop("src", "none")
+      $("#stream").prop("src", "")
     } else {
       $(this).removeClass('goPlay')
       getStream()
